09cc6405ef259c4a0848a7322bdf5437973d00ef
AmanChand <amanchand45@gmail.com>
2016-06-19 22:34:33 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " aman_chand. All rights reserved.</p>";
+
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href=''>Tab 1</a></li>" +
+"<li><a href=''>Tab 2</a></li>" +
+"<li><a href=''>Tab 3</a></li>" +
+"<li><a href=''>Tab 4</a></li>" +
+"</ul>"; 
+
